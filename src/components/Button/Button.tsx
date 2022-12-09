c2a3da100d68f20f7c4256ba19e6c8c696efb92e
amiellion <amielbryangaudia@gmail.com>
2022-12-09 16:51:13 +0800

    [FEAT + FIX] ADD STYLING  - install devDeps typescript-plugin-css-modules  - modify rollup config add preserveModules: true  -  add modules.d.ts in src folder  - add css module for button  -

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from './Button.module.css';
 
 export interface ButtonProps {
     label: string;
@@ -7,7 +7,7 @@ export interface ButtonProps {
 
 
 const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>;
+    return <button className={styles["button"]}>{props.label}</button>;
 };
 
 export default Button;
