fde062222d2ed58014c27b70a0084d5de47aa267
amiellion <amielbryangaudia@gmail.com>
2022-12-09 17:21:30 +0800

    [FEAT + FIX] ADD STYLING  - install rollup-plugin-postcss  - modify rollup config add plugin  postcss(),  - add css module for button  -

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,18 +4,23 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
+// NEW
+import postcss from "rollup-plugin-postcss";
 
 export default [
     {
-        preserveModules: true,
+
         input: "src/index.ts",
         output: [
+
             {
+                preserveModules: true,
                 file: packageJson.main,
                 format: "cjs",
                 sourcemap: true,
             },
             {
+                preserveModules: true,
                 file: packageJson.module,
                 format: "esm",
                 sourcemap: true,
@@ -25,11 +30,17 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+
+            // NEW
+            postcss(),
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+
+        // NEW
+        external: [/\.css$/],
     },
 ];
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Button.module.css';
-import './button.css';
 
 export interface ButtonProps {
     label: string;
@@ -8,7 +7,7 @@ export interface ButtonProps {
 
 
 const Button = (props: ButtonProps) => {
-    return <button className={"button"}>{props.label}</button>;
+    return <button className={styles["button"]}>{props.label}</button>;
 };
 
 export default Button;
