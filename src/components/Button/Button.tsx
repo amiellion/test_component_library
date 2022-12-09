f0ca8de0e610b580a857659a7bcd96aaf2a9342f
amiellion <amielbryangaudia@gmail.com>
2022-12-09 16:57:22 +0800

    [FIX] styling not showing

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Button.module.css';
+import './button.css';
 
 export interface ButtonProps {
     label: string;
@@ -7,7 +8,7 @@ export interface ButtonProps {
 
 
 const Button = (props: ButtonProps) => {
-    return <button className={styles["button"]}>{props.label}</button>;
+    return <button className={"button"}>{props.label}</button>;
 };
 
 export default Button;
